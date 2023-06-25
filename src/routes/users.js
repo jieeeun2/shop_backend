f472aba7848b75bec95d9f246a3bee5cdb4dc077
jieeeun2 <jieun2.dev@gmail.com>
2023-06-25 20:36:00 +0900

    JWT 이용한 인가(Authorization), 로그아웃 기능 구현, navbar 생성

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,18 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 const jwt = require('jsonwebtoken')
+const auth = require('../middleware/auth')
+
+router.get('/auth', auth, (req, res) => {
+  return res.json({
+    id: req.user.id,
+    email: req.user.email,
+    name: req.user.name,
+    password: req.user.password,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
 
 router.post('/register', async (req, res, next) => {
   //유저데이터를 저장
@@ -14,18 +26,16 @@ router.post('/register', async (req, res, next) => {
   }
 })
 
-router.post('/login', async(req, res, next) => {
+router.post('/login', async (req, res, next) => {
   try{
     //존재하는 유저인지 체크
     const user = await User.findOne({email: req.body.email})
-    
     if(!user) {
       return res.status(400).send('Auth failed, email not found')
     }
 
     //비밀번호가 일치하는지 체크
     const isMatch = await user.comparePassword(req.body.password)
-
     if(!isMatch) {
       return res.status(400).send('Wrong password')
     }
@@ -36,12 +46,20 @@ router.post('/login', async(req, res, next) => {
     }
     const accessToken = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1h'})
 
-    console.log('히히', user, accessToken)
     return res.json({user, accessToken})
     
   }catch(error) {
     next(error)
   }
 })
+
+router.post('/logout', auth, async (req, res, next) => {
+  try{
+    return res.sendStatus(200)
+  }catch(error) {
+    next(error)
+  }
+})
+
 
 module.exports = router
